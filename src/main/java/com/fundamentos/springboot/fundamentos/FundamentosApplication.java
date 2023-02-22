38944862908616aac35c65c313d59320961f3518
Juan Ramírez <juan.ramirez@quind.io>
2023-02-22 17:07:45 -0500

    Configuraciones bases queda en JPA

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,7 +1,11 @@
 package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,12 +15,20 @@ import org.w3c.dom.ls.LSOutput;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	private final Log LOGGER= LogFactory.getLog(FundamentosApplication.class);
+
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 
-	public FundamentosApplication(@Qualifier("componentImplementTwo")ComponentDependency componentDependency, MyBean myBean){
+	private MyBeanWithProperties myBeanWithProperties;
+
+	private UserPojo userPojo;
+
+	public FundamentosApplication(@Qualifier("componentImplementTwo")ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency=componentDependency;
 		this.myBean=myBean;
+		this.myBeanWithProperties=myBeanWithProperties;
+		this.userPojo=userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -27,5 +39,14 @@ public class FundamentosApplication implements CommandLineRunner {
 	public void run(String... args) {
 		componentDependency.saludar();
 		myBean.print();
+		System.out.println(myBeanWithProperties.function());
+		System.out.printf(userPojo.getAge()+ " prueba");
+
+		try{
+			//error
+		}catch (Exception e){
+			LOGGER.error("Esto es un error del aplicativo "+ e);
+		}
+
 	}
 }
