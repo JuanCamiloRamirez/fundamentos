275399c26da50c9514b25d63ff5003343fc92898
Camilo Ramirez <juancamilor26@hotmail.com>
2023-02-21 23:43:41 -0500

    prueba

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,12 +2,15 @@ package com.fundamentos.springboot.fundamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class FundamentosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
+
+		System.out.println("hellow");
 	}
 
 }
